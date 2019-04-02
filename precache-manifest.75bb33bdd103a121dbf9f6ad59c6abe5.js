284b114800e1a96b84b8406727f7871a4db32980
Erzhuyijian <1821344102@qq.com>
2019-04-02 20:07:53 +0800

    Updates

---
+++ precache-manifest.75bb33bdd103a121dbf9f6ad59c6abe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  },
+  {
+    "revision": "91ec3a5a9a983fede206",
+    "url": "/first-react-app/static/js/main.a6a8fd2a.chunk.js"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/first-react-app/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "91ec3a5a9a983fede206",
+    "url": "/first-react-app/static/css/main.ba0785d2.chunk.css"
+  },
+  {
+    "revision": "db515e4391c9dfba8e11394eddbf49ec",
+    "url": "/first-react-app/index.html"
+  }
+];
